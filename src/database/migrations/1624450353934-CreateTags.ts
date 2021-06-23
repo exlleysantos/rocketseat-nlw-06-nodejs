a66577f58383a8371b0d8565acc6e0ef22550cd2
Exlley Santos <exlleyclemente@gmail.com>
2021-06-23 09:24:57 -0300

    Create Tag Migration and add midlleware error

---
+++ src/database/migrations/1624450353934-CreateTags.ts
@@ -0,0 +1,40 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTags1624450353934 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "tags",
+                columns: [
+                    {
+                        name:"id",
+                        type:"varchar",
+                        isPrimary: true
+                    },
+                    {
+                        name:"name",
+                        type:"varchar"
+                    },
+                    {
+                        name:"create_at",
+                        type:"timestamp",
+                        default:"now()"
+                    },
+                    {
+                        name:"updated_at",
+                        type:"timestamp",
+                        default:"now()"
+                    }
+                ]
+            })
+        )
+
+        
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("tags");
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors"
 
 import {router} from './routes'
 
@@ -10,8 +11,19 @@ const app = express();
 
 app.use(express.json());
 
-app.use(router)
+app.use(router);
 
+app.use((err: Error, request: Request, response:Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "Error",
+        message: "Internal Server Error"
+    })
+})
 
 app.listen(3000, () => {
     console.log("🔧 Server is running in port 3000!");
